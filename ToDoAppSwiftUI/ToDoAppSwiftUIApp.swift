dbb789f0c3e4c0fcf78007cee4d7d479e22aa70b
Álvaro Ferrández <alvaro.ferrandez.dev@gmail.com>
2021-12-27 10:18:36 +0100

    Initial Commit

---
+++ ToDoAppSwiftUI/ToDoAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppSwiftUIApp.swift
+//  ToDoAppSwiftUI
+//
+//  Created by Álvaro Ferrández Gómez on 27/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
